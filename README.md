8600673aa6da71b0336d6adac7c52b835d037d1b
Jeffrey Scott <valenpendragon@gmail.com>
2023-01-29 09:45:23 -0500

    Add columns to display names, roles, and photos of employees. Update README.md with instructions regarding the data file structure and location of images.

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ Included are two pages. The first is a combination of blurb and organization. Th
 This program is suitable for small companies and organizations.
 
 It requires a spreadsheet with the company employees and a text file with the blurb.
+
+The field structure of each row of data.csv or data.xlsx must be:
+1. first name
+2. last name
+3. role
+4. image
+
+These must be included in the header row as well.
+
+All company images need to be stored in a subdirectory, images.
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas
 import openpyxl
+import os.path
 
 st.set_page_config(layout="wide")
 
@@ -12,3 +13,40 @@ with open("blurb.txt") as file:
 st.write(blurb)
 
 st.header("Our Team")
+
+data_col1, space_col1, data_col2, space_col2, data_col3, space_col3 = st.columns([1.0, 0.5,1.0, 0.5, 1.0, 0.5])
+
+# Look to see which file is present, data.csv, data.xls, or data.xlsx.
+if os.path.isfile("data.csv"):
+    df = pandas.read_csv("data.csv")
+elif os.path.isfile("date.xls"):
+    df = pandas.read_excel("data.xls")
+elif os.path.isfile():
+    df = pandas.read_excel("data.xlsx")
+
+with data_col1:
+    for index, row in df[0::3].iterrows():
+        first_name = row['first name'].lower().capitalize()
+        last_name = row['last name'].lower().capitalize()
+        st.header(f"{first_name} {last_name}")
+        role = row['role'].lower().title()
+        st.write(role)
+        st.image("images/" + row['image'])
+
+with data_col2:
+    for index, row in df[1::3].iterrows():
+        first_name = row['first name'].lower().capitalize()
+        last_name = row['last name'].lower().capitalize()
+        st.header(f"{first_name} {last_name}")
+        role = row['role'].lower().title()
+        st.write(role)
+        st.image("images/" + row['image'])
+
+with data_col3:
+    for index, row in df[2::3].iterrows():
+        first_name = row['first name'].lower().capitalize()
+        last_name = row['last name'].lower().capitalize()
+        st.header(f"{first_name} {last_name}")
+        role = row['role'].lower().title()
+        st.write(role)
+        st.image("images/" + row['image'])
