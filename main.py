4c1df18f157c9e3fcaf35dc2d1c3824b0f632f19
Jeffrey Scott <valenpendragon@gmail.com>
2023-01-29 09:01:41 -0500

    Add company name, blurb, and team header.

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ import streamlit as st
 import pandas
 import openpyxl
 
+st.set_page_config(layout="wide")
+
+st.title("The Best Company")
+
+with open("blurb.txt") as file:
+    blurb = file.read()
+
+st.write(blurb)
+
+st.header("Our Team")
